133ac4915b992994d8807855864369ace2acaf03
Lucas Arvelo <lucasarvelo@gmail.com>
2018-12-07 20:33:39 -0500

    Multiple Components in Sync, Lifting the State Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,10 @@ class NavBar extends Component {
     return (
       <nav class="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Navber{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
